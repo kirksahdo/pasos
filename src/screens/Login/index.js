2308029baee0a71c95040b1f48aaad2ab8492468
Pedro <pedroh93601@gmail.com>
2021-10-07 08:50:00 -0400

    set navigation initial configs

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Text, View, StatusBar, ImageBackground, Image, TextInput, TouchableWithoutFeedback, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StatusBar, ImageBackground, Image, TextInput, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
 
 import background from '../../../assets/background.png';
 import logo from '../../../assets/logo-preta-2.png';
@@ -7,38 +7,38 @@ import crise from '../../../assets/esta-tendo-uma-crise.png';
 
 import styles from './styles';
 
-const Login = () => {
+const Login = ({ navigation }) => {
 
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
 
 
-    async function login(){
-        
+    async function login() {
+
     }
 
 
-    return(
-        <ImageBackground source={background} style={ styles.background }>
-            <StatusBar translucent backgroundColor="transparent" />    
+    return (
+        <ImageBackground source={background} style={styles.background}>
+            <StatusBar translucent backgroundColor="transparent" />
 
             <View style={styles.logo}>
-                <Image source={logo} style={{margin: 20}} />
+                <Image source={logo} style={{ margin: 20 }} />
             </View>
             <View style={styles.form}>
                 <View style={styles.header} >
                     <Text style={styles.headerTitle}>Login</Text>
                 </View>
-                <View style={styles.inputs}> 
-                    <TextInput style={styles.txtInput} placeholder='EMAIL' value = {email} onChangeText={txt => setEmail(txt)}  />
-                    <TextInput style={styles.txtInput} placeholder='SENHA' value = {senha} onChangeText={txt => setSenha(txt)}  />
+                <View style={styles.inputs}>
+                    <TextInput style={styles.txtInput} placeholder='EMAIL' value={email} onChangeText={txt => setEmail(txt)} />
+                    <TextInput style={styles.txtInput} placeholder='SENHA' value={senha} onChangeText={txt => setSenha(txt)} />
                 </View>
-                <View style= {styles.buttons}> 
+                <View style={styles.buttons}>
                     <TouchableWithoutFeedback >
                         <Text style={styles.txtExtras}>Esqueceu sua senha?</Text>
                     </TouchableWithoutFeedback>
                     <TouchableWithoutFeedback>
-                        <Text style={styles.txtExtras}>Ainda não é cadastrado?</Text>
+                        <Text style={styles.txtExtras} onPress={() => navigation.navigate('Register')}  >Ainda não é cadastrado?</Text>
                     </TouchableWithoutFeedback>
                     <TouchableOpacity style={styles.btnEntrar} onPress={login}>
                         <Text style={styles.labelEntrar}>ENTRAR</Text>
@@ -49,7 +49,7 @@ const Login = () => {
                 <Image source={crise} />
             </TouchableOpacity>
         </ImageBackground>
-            
+
     );
 
 }
